17ab5ac4f05e13df68c111f2c628c9a7da90aab6
Mikhail Rak <misha.rak.2401@gmail.com>
2023-02-12 18:27:33 +0200

    added second task

--- homeWork8/js/index.js
+++ homeWork8/js/index.js
@@ -11,6 +11,31 @@ function sumTwoSmallestNumbers(...args) {
   }
 }
 
-console.log(sumTwoSmallestNumbers(19, 5, 42, 2, 77)); // 7
-console.log(sumTwoSmallestNumbers(1, 3, 2)); // 3
-console.log(sumTwoSmallestNumbers(1)); // undefined и отдельный console.error("...")
+// console.log(sumTwoSmallestNumbers(19, 5, 42, 2, 77)); // 7
+// console.log(sumTwoSmallestNumbers(1, 3, 2)); // 3
+// console.log(sumTwoSmallestNumbers(1)); // undefined и отдельный console.error("...")
+
+//task2
+function createCalculator(a) {
+  let number = a;
+  return {
+    sum: function (x) {
+      return (number = number + x);
+    },
+    mult: function (x) {
+      return (number = number * x);
+    },
+    sub: function (x) {
+      return (number = number - x);
+    },
+    div: function (x) {
+      return (number = number / x);
+    },
+  };
+}
+
+const calc = createCalculator(10);
+console.log(calc.sum(5)); /// 15
+console.log(calc.mult(10)); // 150
+console.log(calc.sub(40)); // 110
+console.log(calc.div(10)); // 11
