fe9390413f11eff9332b1bedc055e7e8a6f286de
Mikhail Rak <misha.rak.2401@gmail.com>
2023-02-14 11:10:16 +0200

    added first task

---
+++ homeWork9/js/index.js
@@ -0,0 +1,9 @@
+// "use strick";
+// //task1
+let dayOfMybirthday = new Date("1997-01-24");
+let day = dayOfMybirthday.getFullYear;
+console.log(
+  `${dayOfMybirthday.getDate()}.${
+    dayOfMybirthday.getMonth() + 1
+  }.${dayOfMybirthday.getFullYear()}`
+);
